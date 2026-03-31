c07fb1334aab96f993c67a77603aa5b08ce954f0
Suyash1855 <103357490+Suyash1855@users.noreply.github.com>
2026-03-31 12:39:17 +0530

    Update main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -13,10 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://house-price-predict-ml.vercel.app",
-    ],  # Vite dev server
+    allow_origins=["*"],  # Vite dev server
     allow_methods=["*"],
     allow_headers=["*"],
 )
